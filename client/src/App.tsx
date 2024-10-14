bc77b969dd0bd6ef9a2e0a65f18edd5d67cdff2b
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-10-14 20:43:05 -0300

    vini

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+// @deno-types="@types/react"
+import { useState } from "react";
+// @ts-expect-error Unable to infer type at the moment
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>RPG dos Brodi irá ocorrer dia 02/11</h1>
+        </>
+    );
+}
+
+export default App;
